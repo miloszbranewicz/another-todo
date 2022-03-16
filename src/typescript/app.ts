202c625e0824e3dd9e8e972aab6d0f82484f667a
Milosz <m.branewicz@wp.pl>
2022-03-16 22:38:20 +0100

    FEAT:Adding todo - Ver. 2

--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -1,12 +1,13 @@
-import { Todo } from "./classes/Todo";
+import { Todo } from "./classes/Todo.js";
 const todoForm: HTMLFormElement = document.getElementById('todo-form') as HTMLFormElement;
 todoForm.addEventListener('submit', (e: Event) => {
     e.preventDefault()
     const todoListing: HTMLUListElement = document.getElementById('todo-listing') as HTMLUListElement;
     const todoInput: HTMLInputElement = document.getElementById('todo-form-title') as HTMLInputElement;
     const todoInputValue:string = todoInput.value;
-
-    todoListing.innerHTML = new Todo(todoInputValue).getTodoItem;
+    const x = new Todo(todoInputValue)
+    console.log(x)
+    todoListing.appendChild(new Todo(todoInputValue).todoElement)
 
 
 })
--- src/typescript/classes/Todo.ts
+++ src/typescript/classes/Todo.ts
@@ -1,43 +1,32 @@
 export class Todo {
-    title: string | null;
-    ICON_DELETE: string = `<svg class="todo__icon-delete" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  title: string | null;
+  ICON_DELETE = `<svg class="todo__icon-delete" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>`;
-    ICON_DONE: string = `<svg class="todo__icon-done" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  ICON_DONE = `<svg class="todo__icon-done" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
                                     </svg>`;
-    TODO_ITEM: string
-    constructor(title:string) {
-        this.title = title;
-        this.TODO_ITEM = `<li class="todo">
-                            <h2 class="todo__title">${this.title}</h2>
-                            <div class="todo__icons">
-                                ${this.ICON_DELETE}
-                                ${this.ICON_DONE}
-                            </div>
-                        </li>`
-    }
-    set setIconDelete(iconSvgCode: string) {
-        this.ICON_DELETE = iconSvgCode;
-    }
-    get getIconDelete() {
-        return this.ICON_DELETE;
-    }
+  todoElement: HTMLLIElement;
+  constructor(title: string) {
+    this.title = title;
+    this.todoElement = this.createTodo();
+  }
+  private createTodo() {
+    const liElement = document.createElement("li");
+    liElement.classList.add("todo");
+    const todoHeader = document.createElement("h2");
+    todoHeader.classList.add("todo__title");
+    todoHeader.textContent = this.title;
+    const todoIconsContainer = document.createElement("div");
+    todoIconsContainer.classList.add("todo__icons");
+    todoIconsContainer.innerHTML = `${this.ICON_DELETE + this.ICON_DONE}`;
 
-    set setIconde(iconSvgCode: string) {
-        this.ICON_DONE = iconSvgCode;
-    }
-    get getIconDone() {
-        return this.ICON_DONE;
-    }
+    liElement.appendChild(todoHeader);
+    liElement.appendChild(todoIconsContainer);
+    return liElement;
+  }
 
-    set setTitle(title: string) {
-        this.title = title;
-    }
-    get getTitle() {
-        return this.title;
-    }
-    get getTodoItem() {
-        return this.TODO_ITEM;
-    }
+  get getTodoElement() {
+    return this.todoElement;
+  }
 }
